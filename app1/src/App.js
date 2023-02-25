3f1470d382574872c79a36dbdaa858fe5bfed3dd
KianaMo <kiana.montazeri@myy.haaga-helia.fi>
2023-02-25 15:17:49 +0200

    todo list without delete

---
+++ app1/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import React from 'react';
+import './index.css';
+import TodoList from './TodoList';
+
+function App() {
+
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
